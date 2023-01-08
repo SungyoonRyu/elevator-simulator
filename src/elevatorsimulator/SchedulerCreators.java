965e6e8dc637580024ac6e1e0bafc442c3f4f042
SungyoonRyu <rara0620@naver.com>
2023-01-09 05:48:13 +0900

    feat: add some algorithms

--- src/elevatorsimulator/SchedulerCreators.java
+++ src/elevatorsimulator/SchedulerCreators.java
@@ -1,2 +1,51 @@
-package elevatorsimulator;public class SchedulerCreators {
+package elevatorsimulator;
+
+import elevatorsimulator.schedulers.LongestQueueFirst;
+import elevatorsimulator.schedulers.RoundRobin;
+import elevatorsimulator.schedulers.ThreePassageGroupElevator;
+import elevatorsimulator.schedulers.Zoning;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SchedulerCreators {
+    public static final List<SchedulerCreator> creators = new ArrayList<SchedulerCreator>();
+
+    static {
+        creators.add(new SchedulerCreator() {
+            @Override
+
+            public SchedulingAlgorithm createScheduler(Building building) {
+                return new LongestQueueFirst();
+            }
+        });
+
+        creators.add(new SchedulerCreator() {
+            @Override
+            public SchedulingAlgorithm createScheduler(Building building) {
+                return new Zoning(building.getElevatorCars().length, building);
+            }
+        });
+
+        creators.add(new SchedulerCreator() {
+            @Override
+            public SchedulingAlgorithm createScheduler(Building building) {
+                return new RoundRobin(building, false);
+            }
+        });
+
+        creators.add(new SchedulerCreator() {
+            @Override
+            public SchedulingAlgorithm createScheduler(Building building) {
+                return new RoundRobin(building, true);
+            }
+        });
+
+        creators.add(new SchedulerCreator() {
+            @Override
+            public SchedulingAlgorithm createScheduler(Building building) {
+                return new ThreePassageGroupElevator(building);
+            }
+        });
+    }
 }
